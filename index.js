a6da57ccb1455ee0f81bb132c62132f92790aae5
halla <aniabisso.16@gmail.com>
2023-07-23 16:29:40 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+const numbersSet = [5, 10, 15, 20];
+const max = Math.max(...numbersSet);
+
+module.exports = { multiply, random, mod, max };
+
